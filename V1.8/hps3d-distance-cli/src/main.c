8dfb3b82832f21de4864b647c3c69a8fc0457bd4
Marcus Neuert <mneuert@online.de>
2025-07-12 00:30:04 +0200

    added callback function

--- V1.8/hps3d-distance-cli/src/main.c
+++ V1.8/hps3d-distance-cli/src/main.c
@@ -7,6 +7,41 @@
 static int g_handle = -1;
 static HPS3D_MeasureData_t g_measureData;
 
+static bool isReconnectEnable = true;
+static bool isReconnect = false;
+static void EventCallBackFunc(int handle, int eventType, uint8_t *data,int dataLen, void *userPara)
+{
+    switch ((HPS3D_EventType_t)eventType)
+    {
+        // Measurement data notification event
+        case HPS3D_SIMPLE_ROI_EVEN:	
+        case HPS3D_FULL_ROI_EVEN:	
+        case HPS3D_FULL_DEPTH_EVEN:
+        case HPS3D_SIMPLE_DEPTH_EVEN:
+            printf("handle:%d!\n", handle);
+            HPS3D_ConvertToMeasureData(data,&g_measureData, (HPS3D_EventType_t)eventType);
+            PrintResultData((HPS3D_EventType_t)eventType, g_measureData);
+            break;
+        case HPS3D_SYS_EXCEPTION_EVEN: /* System exception notification event */
+            printf("SYS ERR :%s\n", data);
+
+            break;
+        case HPS3D_DISCONNECT_EVEN: /* Connection abnormal disconnection notification event */
+            printf("Device disconnected!\n");
+            //sleep(10);
+            //HPS3D_StopCapture(handle);
+            //sleep(10);
+            if(isReconnectEnable && isReconnect == false)
+            {
+                isReconnect = true;
+            }
+            break;
+        case HPS3D_NULL_EVEN:  // Null event notification
+        default:
+            break;	
+    }
+}
+
 void signal_handler(int sig)
 {
     char c;
@@ -69,6 +104,11 @@ int main(int argc, char *argv[]) {
     int pixel_x = atoi(argv[1]);
     int pixel_y = atoi(argv[2]);
 
+    //debug
+    printf("pixel_x=%d, pixel_y=%d\n", pixel_x, pixel_y);
+
+    HPS3D_StatusTypeDef ret = HPS3D_RET_OK;
+
     signal(SIGINT, signal_handler);
 
     // Initialisiere Messdatenstruktur
@@ -80,9 +120,20 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+		// Register event callback function to receive continuous return packets and handle exceptions;
+    ret= HPS3D_RegisterEventCallback(EventCallBackFunc, NULL);
+    if (ret != HPS3D_RET_OK)
+    {
+        printf("Failed to register callback function, Err:%d\n", ret);
+        break;
+
+    }
+
     query_distance(pixel_x, pixel_y);
 
     HPS3D_StopCapture(g_handle);
     HPS3D_CloseDevice(g_handle);
+    HPS3D_MeasureDataFree(&g_measureData);
+    printf("Device disconnected!\n\n");
     return 0;
 }
